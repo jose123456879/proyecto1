6904af1c418a1a845f2d501923e3f4f93504cf37
jose123456879 <totitosst@gmail.com>
2016-05-27 08:21:28 -0600

    prob 4 y quick fix

--- proyecto1.py
+++ proyecto1.py
@@ -6,7 +6,17 @@ def prob_2(F):
 	c=(F-32)*5/9
 	return c
 
-def prb_3(B,P):
-	r=B**P
+def prob_3(B,P):
+	r=B
+	for i in range (P-1):
+		r=B*P
 	return r
+
+def prob_4(n,F):
+	h=list(F)
+	largo=len(F)
+	jamon=(n-largo)//2
+	r=("*"*jamon+F+"*"*jamon)
+	return r
+	
 
